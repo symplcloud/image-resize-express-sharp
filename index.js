69eca1cb47d25fd80a2a0f451766c0eefa284c6c
Sean Lee <seanlee@sympl.cloud>
2019-09-24 22:27:09 -0400

    enable static files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   sharp('input.jpg')
     .resize({ width: 320 })
